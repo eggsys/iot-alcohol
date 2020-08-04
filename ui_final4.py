5b6664a0b7cbd5362c8f20b2418c8e08a8eb506b
eggsys <46551235+eggsys@users.noreply.github.com>
2020-08-04 10:34:30 +0700

    # Comment

--- ui_final4.py
+++ ui_final4.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_Form(object):
-
+#button functions
     def clicked_back(self, nd):
         print("click : go back")
         Form.pageX = - 1
